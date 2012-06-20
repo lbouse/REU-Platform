e639e994c13df7db0851ed5c22392a18b4a0caf2
Bouse <Bouse@192.168.196.192>
2012-06-20 15:56:03 -0500

    Testing commit

---
+++ REU_Platform/src/reu_platform/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reu_platform;
+
+/**
+ *
+ * @author Bouse
+ */
+public class NewClass {
+    //TEST.
+}
